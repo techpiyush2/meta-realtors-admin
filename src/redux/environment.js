00366c574d0dc2ede55376c245af107bcfe9d9f0
piyush <piyush@gmail.com>
2023-09-27 18:02:19 +0530

    changes done

--- src/redux/environment.js
+++ src/redux/environment.js
@@ -1,6 +1,6 @@
 export default {
-    // baseUrl: 'https://admin.metarealtors.in/',
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: 'https://admin.metarealtors.in/',
+    // baseUrl: 'http://localhost:5000/',
 
 //   
 }
